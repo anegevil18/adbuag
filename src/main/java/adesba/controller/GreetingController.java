30ca7d38d3832422e19e036b1134a2a3074a24fe
anegevil18 <anegevil18@gmail.com>
2018-09-24 15:48:08 -0500

    cambio a configuracion thymeleaf

--- src/main/java/adesba/controller/GreetingController.java
+++ src/main/java/adesba/controller/GreetingController.java
@@ -3,13 +3,10 @@ package adesba.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.time.LocalDate;
 
 @Controller
 public class GreetingController {
-
-
 
     @RequestMapping(value = {"/"})
     public String viewHomePage(Model model) {
