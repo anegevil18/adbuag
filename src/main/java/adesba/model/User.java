11a7bba0b9d7ff598d3f24611c230b068c4b3cf4
anegevil18 <anegevil18@gmail.com>
2018-11-12 09:51:32 -0600

    cambio a configuracion thymeleaf

---
+++ src/main/java/adesba/model/User.java
@@ -0,0 +1,35 @@
+package adesba.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.util.Date;
+@Entity
+public class User implements Serializable {
+    private String correo;
+    private String username;
+    private String pwd;
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
